bec69ff9b9553bf2db992e66ea4868815b37c64c
forsrc <forsrc>
2016-12-20 20:00:03 +0900

    init

---
+++ src/main/java/com/forsrc/MySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.forsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+}
