699e03a2d20acadedaec2add7f2e7083e5265706
forsrc <forsrc@163.com>
2017-01-26 00:50:00 +0900

    jsLoad

--- src/main/webapp/static/js/web/jsLoad.js
+++ src/main/webapp/static/js/web/jsLoad.js
@@ -21,7 +21,8 @@
                 var url = target.getAttribute("data-url");
                 var data = target.getAttribute("data-params");
                 ajax(url, data, function(html) {
-                    target.innerHTML = html;
+                    //target.innerHTML = html;
+                    innerHtml(target, html);
                 });
             });
 
@@ -50,6 +51,10 @@
                     } catch (e) {
                     }
                 }
+                if (data && typeof data === "string") {
+                    data += data.indexOf("?") > 0 ? "&" : "?";
+                    data += "function=JsLoad";
+                }
                 Ajax.send({
                     type: "GET",
                     url: url,
@@ -67,11 +72,50 @@
                     }
                 });
             }
+
+            function innerHtml(target, text) {
+                target.innerHTML = text;
+                var scripts = [];
+
+                var ret = target.childNodes;
+                for (var i = 0; ret[i]; i++) {
+                    if (scripts && nodeName(ret[i], "script") && (!ret[i].type
+                        || ret[i].type.toLowerCase() === "text/javascript")) {
+                        scripts.push(ret[i].parentNode ? ret[i].parentNode.removeChild(ret[i]) : ret[i]);
+                    }
+                }
+
+                for (var script in scripts) {
+                    evalScript(scripts[script]);
+                }
+            }
+
+            function nodeName(elem, name) {
+                return elem.nodeName && elem.nodeName.toUpperCase() === name.toUpperCase();
+            }
+
+            function evalScript(elem) {
+                var data = ( elem.text || elem.textContent || elem.innerHTML || "" );
+
+                var head = document.getElementsByTagName("head")[0] || document.documentElement,
+                    script = document.createElement("script");
+                script.type = "text/javascript";
+                script.appendChild(document.createTextNode(data));
+                head.insertBefore(script, head.firstChild);
+                head.removeChild(script);
+
+                if (elem.parentNode) {
+                    elem.parentNode.removeChild(elem);
+                }
+            }
             ;
         };
         return {
             load: function(className, node, tag) {
                 load(className, node, tag);
+            },
+            innerHtml: function (target, html) {
+                innerHtml(target, html);
             }
         };
     });
