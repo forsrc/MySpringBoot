6d2e4f8a0ccc39cff715ca8eceacc34089f4781a
forsrc <forsrc@163.com>
2017-02-07 00:28:14 +0900

    ehcache

--- src/main/java/com/forsrc/boot/config/InitDbConig.java
+++ src/main/java/com/forsrc/boot/config/InitDbConig.java
@@ -1,34 +1,46 @@
 package com.forsrc.boot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InitDbConig {
 
-    @Bean
-    public InitDbConig init(@Autowired EntityManager entityManager) {
-        System.out.println("--> entityManager :" + entityManager);
-        Query query = null;
-        try {
-            query = entityManager.createNamedQuery("sql_user_init");
-            query.executeUpdate();
-            entityManager.flush();
-            query = entityManager.createNamedQuery("sql_user_privacy_init");
-            query.executeUpdate();
-            entityManager.flush();
-            query = entityManager.createNamedQuery("sql_role_init");
-            query.executeUpdate();
-            entityManager.flush();
-            query = entityManager.createNamedQuery("sql_user_role_init");
-            query.executeUpdate();
-            entityManager.flush();
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
+    @Component
+    class InitDb {
+
+
+        @Autowired
+        public EntityManager entityManager;
+
+        public InitDb() {
+            init();
         }
-        return this;
+
+        @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+        public void init() {
+
+            Query query = null;
+            try {
+                query = entityManager.createNamedQuery("sql_user_init");
+                query.executeUpdate();
+                query = entityManager.createNamedQuery("sql_user_privacy_init");
+                query.executeUpdate();
+                query = entityManager.createNamedQuery("sql_role_init");
+                query.executeUpdate();
+                query = entityManager.createNamedQuery("sql_user_role_init");
+                query.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+
 }
