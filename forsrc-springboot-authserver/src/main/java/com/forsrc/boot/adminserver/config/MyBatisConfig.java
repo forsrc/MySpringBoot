60b2252e1e0d35a1e68a196a1178e7ba3e991429
forsrc <forsrc@163.com>
2017-06-11 03:11:22 +0900

    mybatis

---
+++ forsrc-springboot-authserver/src/main/java/com/forsrc/boot/adminserver/config/MyBatisConfig.java
@@ -0,0 +1,34 @@
+package com.forsrc.boot.adminserver.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan("com.forsrc.core.web.user.dao.mapper")
+public class MyBatisConfig {
+    
+    @Autowired
+    @Qualifier("primaryDataSource")
+    private DataSource primaryDataSource;
+
+    //@Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(primaryDataSource);
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath*:com/forsrc/core/web/user/dao/mapper/*Mapper.xml"));
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+}
