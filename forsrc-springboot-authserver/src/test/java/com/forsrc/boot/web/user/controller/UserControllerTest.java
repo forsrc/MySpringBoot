b91cb7d9e111324d490458dc8770a5a35073e7b3
forsrc <forsrc@163.com>
2017-06-12 23:22:58 +0900

    test

--- forsrc-springboot-authserver/src/test/java/com/forsrc/boot/web/user/controller/UserControllerTest.java
+++ forsrc-springboot-authserver/src/test/java/com/forsrc/boot/web/user/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package com.forsrc.boot.web.user.controller;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
-import org.springframework.security.oauth2.client.test.OAuth2ContextSetup;
-import org.springframework.security.oauth2.client.test.RestTemplateHolder;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestOperations;
 
 import com.forsrc.pojo.User;
 
