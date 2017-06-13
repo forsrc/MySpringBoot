6678fb3f2983c9b532163b83282e6f8e538b7b6e
forsrc <forsrc@163.com>
2017-06-13 13:31:29 +0900

    rename forsrc-springcloud-config-server

---
+++ forsrc-springcloud-config-server/src/main/java/com/forsrc/boot/web/test/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.forsrc.boot.web.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping(value = "/test")
+    public ResponseEntity<String> test(String name) {
+
+        return new ResponseEntity<>("hello world. " + (name == null ? "" : name), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/evn")
+    public ResponseEntity<String> evn(String key) {
+        return new ResponseEntity<>("Environment: " + environment.getProperty(key), HttpStatus.OK);
+    }
+}
