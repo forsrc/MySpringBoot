2eaf6bc56d053f3c9cf816b680ce70e5fc9c9e75
forsrc <forsrc@163.com>
2017-06-13 17:34:15 +0900

    forsrc-springcloud-eureka-client

---
+++ forsrc-springcloud-eureka-client/src/main/java/com/forsrc/boot/web/test/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.forsrc.boot.web.test.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/client/getInstances/{name}")
+    public ResponseEntity<List<ServiceInstance>> getInstances( @PathVariable String name) {
+        return new ResponseEntity<>(this.discoveryClient.getInstances(name), HttpStatus.OK);
+    }
+
+    @RequestMapping("/client/getServices")
+    public ResponseEntity<List<String>> getServices() {
+        return new ResponseEntity<>(this.discoveryClient.getServices(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/test")
+    public ResponseEntity<String> test(String name) {
+
+        return new ResponseEntity<>("hello world. " + (name == null ? "" : name), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/evn")
+    public ResponseEntity<String> evn(String key) {
+        return new ResponseEntity<>("Environment: " + environment.getProperty(key), HttpStatus.OK);
+    }
+}
