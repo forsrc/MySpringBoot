5f9d002992b5f433bc3cbd7ec361e4c9728e9713
forsrc <forsrc@163.com>
2017-06-13 13:02:36 +0900

    springboot-config-server

---
+++ forsrc-springboot-config-server/src/main/java/com/forsrc/boot/configserver/config/ConfigServerConfig.java
@@ -0,0 +1,10 @@
+package com.forsrc.boot.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfig {
+
+}
