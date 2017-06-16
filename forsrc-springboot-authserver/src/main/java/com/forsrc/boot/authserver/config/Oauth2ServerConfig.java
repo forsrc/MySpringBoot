129d91c620dc1e461dfaf00689ac25d27feb2f17
forsrc <forsrc@163.com>
2017-06-17 00:21:31 +0900

    oauth2

--- forsrc-springboot-authserver/src/main/java/com/forsrc/boot/authserver/config/Oauth2ServerConfig.java
+++ forsrc-springboot-authserver/src/main/java/com/forsrc/boot/authserver/config/Oauth2ServerConfig.java
@@ -1,12 +1,6 @@
 package com.forsrc.boot.authserver.config;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,49 +10,30 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
-import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
 @EnableConfigurationProperties({ AuthorizationServerProperties.class })
+@Order(77)
 public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     @Qualifier("authenticationManagerBean")
-    AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
     @Autowired
-    AuthorizationServerProperties authorizationServerProperties;
-    @Autowired
-    private DataSource dataSource;
+    private AuthorizationServerProperties authorizationServerProperties;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -76,49 +51,24 @@ public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer()));
-        // @formatter:off
-        endpoints
-            .accessTokenConverter(jwtAccessTokenConverter())
-            //.authorizationCodeServices(authorizationCodeServices())
-            .tokenEnhancer(tokenEnhancerChain)
-            .tokenStore(jwtTokenStore())
-            .approvalStore(jwtApprovalStore())
-            //.tokenStore(jdbcTokenStore())
-            //.approvalStore(jdbcApprovalStore())
-            .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST)
-            .authenticationManager(authenticationManager)
-        ;
-        // @formatter:on
+    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
     }
 
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    public void configure(final AuthorizationServerEndpointsConfigurer configurer) {
         // @formatter:off
-        security
-            .allowFormAuthenticationForClients()
-            .tokenKeyAccess(authorizationServerProperties.getTokenKeyAccess())
-            .checkTokenAccess("isAuthenticated()")
-            .tokenKeyAccess("permitAll()")
-            .checkTokenAccess("isAuthenticated()")
-            .addTokenEndpointAuthenticationFilter(checkTokenEndpointFilter())
-            ;
+        configurer
+            .tokenStore(jwtTokenStore())
+            .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST)
+            .accessTokenConverter(jwtAccessTokenConverter())
+            .authenticationManager(authenticationManager);
         // @formatter:on
-    }
-
-    @Bean
-    public ClientCredentialsTokenEndpointFilter checkTokenEndpointFilter() {
-        ClientCredentialsTokenEndpointFilter filter = new ClientCredentialsTokenEndpointFilter("/oauth/check_token");
-        filter.setAuthenticationManager(authenticationManager);
-        filter.setAllowOnlyPost(true);
-        return filter;
     }
 
     @Bean
     @ConfigurationProperties("jwt")
-    JwtAccessTokenConverter jwtAccessTokenConverter() {
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
         return new JwtAccessTokenConverter();
     }
 
@@ -126,7 +76,6 @@ public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
     @Primary
     public DefaultTokenServices jwtTokenServices() {
         final DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setSupportRefreshToken(true);
         tokenServices.setTokenStore(jwtTokenStore());
         return tokenServices;
     }
@@ -134,68 +83,6 @@ public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
     @Bean
     public TokenStore jwtTokenStore() {
         return new JwtTokenStore(jwtAccessTokenConverter());
-    }
-
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
-        final DataSourceInitializer initializer = new DataSourceInitializer();
-        initializer.setDataSource(dataSource);
-        initializer.setDatabasePopulator(databasePopulator());
-        return initializer;
-    }
-
-    private DatabasePopulator databasePopulator() {
-        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(new ClassPathResource("classpath:schema.sql"));
-        populator.addScript(new ClassPathResource("classpath:data.sql"));
-        return populator;
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices jdbcTokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(jdbcTokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
-
-    @Bean
-    public TokenEnhancer tokenEnhancer() {
-        return new TokenEnhancer() {
-            @Override
-            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-                final Map<String, Object> additionalInfo = new HashMap<>();
-                additionalInfo.put("organization", authentication.getName() + UUID.randomUUID());
-                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
-                return accessToken;
-            }
-        };
-    }
-
-
-    @Bean
-    public JdbcTokenStore jdbcTokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
-
-    @Bean
-    public AuthorizationCodeServices authorizationCodeServices() {
-        return new JdbcAuthorizationCodeServices(dataSource);
-    }
-
-    @Bean
-    public ApprovalStore jdbcApprovalStore() {
-        TokenApprovalStore tokenApprovalStore = new TokenApprovalStore();
-        tokenApprovalStore.setTokenStore(jdbcTokenStore());
-        return tokenApprovalStore;
-    }
-    
-    @Bean
-    public ApprovalStore jwtApprovalStore() {
-        TokenApprovalStore tokenApprovalStore = new TokenApprovalStore();
-        tokenApprovalStore.setTokenStore(jwtTokenStore());
-        return tokenApprovalStore;
     }
 
 }
