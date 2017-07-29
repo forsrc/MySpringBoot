f2eef8e6aa88027529294987eeffbe2c25e7bec6
forsrc <forsrc@163.com>
2017-07-29 16:37:01 +0900

    spring-batch

--- forsrc-springboot-batch/src/main/java/com/forsrc/boot/batch/rest/BatchTargetJobLauncher.java
+++ forsrc-springboot-batch/src/main/java/com/forsrc/boot/batch/rest/BatchTargetJobLauncher.java
@@ -1,14 +1,13 @@
 package com.forsrc.boot.batch.rest;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -24,32 +23,31 @@ public class BatchTargetJobLauncher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BatchTargetJobLauncher.class);
 
-    private final Job job;
-
-    private final JobLauncher jobLauncher;
+    @Autowired
+   // @Qualifier("batchTargetJob")
+    @Qualifier("job1")
+    private Job job;
 
     @Autowired
-    public BatchTargetJobLauncher(@Qualifier("batchTargetJob") Job job, JobLauncher jobLauncher) {
-        this.job = job;
-        this.jobLauncher = jobLauncher;
-    }
+    private JobLauncher jobLauncher;
 
     @Scheduled(cron = "${api.batch.target.cron}")
     public void doMain() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException,
             JobInstanceAlreadyCompleteException {
         LOGGER.info("----> START");
-
-        jobLauncher.run(job, newExecution());
-
+        JobExecution jobExecution = jobLauncher.run(job, batchTargetJobParameters());
+        LOGGER.info("{}", jobExecution);
         LOGGER.info("----> END");
     }
 
-    private JobParameters newExecution() {
-        Map<String, JobParameter> parameters = new HashMap<>();
+    private JobParameters batchTargetJobParameters() {
 
-        JobParameter parameter = new JobParameter(new Date());
-        parameters.put("currentTime", parameter);
+        // @formatter:off
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addDate("now", new Date())
+                .toJobParameters();
+        // @formatter:on
 
-        return new JobParameters(parameters);
+        return jobParameters;
     }
 }
--- forsrc-springboot-batch/src/main/java/com/forsrc/boot/batch/rest/BatchTargetJonConfig.java
+++ forsrc-springboot-batch/src/main/java/com/forsrc/boot/batch/rest/BatchTargetJonConfig.java
@@ -1,19 +1,31 @@
 package com.forsrc.boot.batch.rest;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.client.RestTemplate;
 
 import com.forsrc.boot.batch.pojo.BatchTarget;
@@ -23,8 +35,17 @@ public class BatchTargetJonConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BatchTargetJonConfig.class);
 
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Bean
-    public ItemReader<BatchTarget> batchTargetItemReader(Environment environment, RestTemplate restTemplate) {
+    public ItemReader<BatchTarget> batchTargetItemReader() {
         return new BatchTargetItemReader(environment.getRequiredProperty("api.batch.target.url"), restTemplate);
     }
 
@@ -39,16 +60,96 @@ public class BatchTargetJonConfig {
     }
 
     @Bean
-    public Step batchTargetStep(ItemReader<BatchTarget> batchTargetItemReader,
-            ItemProcessor<BatchTarget, BatchTarget> batchTargetProcessor, ItemWriter<BatchTarget> batchTargetItemWriter,
-            StepBuilderFactory stepBuilderFactory) {
-        return stepBuilderFactory.get("batchTargetStep").<BatchTarget, BatchTarget>chunk(1).reader(batchTargetItemReader)
-                .processor(batchTargetProcessor).writer(batchTargetItemWriter).build();
+    public JobRepository jobRepository(DataSource dataSource, PlatformTransactionManager transactionManager)
+            throws Exception {
+        JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
+        jobRepositoryFactoryBean.setDataSource(dataSource);
+        jobRepositoryFactoryBean.setTransactionManager(transactionManager);
+        jobRepositoryFactoryBean.setDatabaseType("h2");
+        return jobRepositoryFactoryBean.getObject();
     }
 
     @Bean
-    public Job batchTargetJob(JobBuilderFactory jobBuilderFactory, @Qualifier("batchTargetStep") Step batchTargetStep) {
-        return jobBuilderFactory.get("batchTargetJob").incrementer(new RunIdIncrementer()).flow(batchTargetStep).end()
+    // @formatter:off
+    public Step batchTargetStep() {
+        return stepBuilderFactory
+                .get("batchTargetStep")
+                .<BatchTarget, BatchTarget>chunk(1)
+                .reader(batchTargetItemReader())
+                .processor(batchTargetItemProcessor())
+                .writer(batchTargetItemWriter())
                 .build();
+    }
+    // @formatter:on
+
+    @Bean
+    public Step stepInit() {
+        return stepBuilderFactory.get("stepInit").tasklet(new Tasklet() {
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+                LOGGER.info("stepInit execute() --> {}", contribution);
+                LOGGER.info("stepInit execute() --> {}", chunkContext);
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Step stepDone() {
+        return stepBuilderFactory.get("stepDone").tasklet(new Tasklet() {
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+                LOGGER.info("stepDone execute() --> {}", contribution);
+                LOGGER.info("stepDone execute() --> {}", chunkContext);
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    // @formatter:off
+    public Job batchTargetJob() {
+
+        return jobBuilderFactory
+                .get("batchTargetJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(batchTargetJobExecutionListener())
+                .flow(stepInit())
+                .on(ExitStatus.COMPLETED.getExitCode())
+                .to(batchTargetStep())
+                .on(ExitStatus.COMPLETED.getExitCode())
+                .to(stepDone())
+                .end()
+                .build();
+    }
+    // @formatter:on
+
+    @Bean
+    // @formatter:off
+    public Job job1() {
+        return jobBuilderFactory
+                .get("job1")
+                .preventRestart()
+                .start(stepInit())
+                .next(batchTargetStep())
+                .next(stepDone())
+                .build();
+    }
+    // @formatter:on
+
+    @Bean
+    public JobExecutionListener batchTargetJobExecutionListener() {
+        return new JobExecutionListener() {
+
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                LOGGER.info("beforeJob() --> {}", jobExecution);
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+                LOGGER.info("afterJob() --> {}", jobExecution);
+
+            }
+
+        };
     }
 }
