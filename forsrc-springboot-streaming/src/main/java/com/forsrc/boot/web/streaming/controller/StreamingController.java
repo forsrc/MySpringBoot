804fc4d262543e640327e62cee5be163d8619bf9
yes <forsrc@gmail.com>
2018-01-29 19:05:13 +0900

    Update StreamingController.java

--- forsrc-springboot-streaming/src/main/java/com/forsrc/boot/web/streaming/controller/StreamingController.java
+++ forsrc-springboot-streaming/src/main/java/com/forsrc/boot/web/streaming/controller/StreamingController.java
@@ -2,11 +2,14 @@ package com.forsrc.boot.web.streaming.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.forsrc.boot.web.streaming.service.StreamingService;
@@ -17,11 +20,10 @@ public class StreamingController {
     @Autowired
     private StreamingService service;
 
-    @RequestMapping(path = "/api/batch/target", method = RequestMethod.GET)
-    public ResponseEntity<List<String>> list() {
-       
-        return new ResponseEntity<>(service.in(), HttpStatus.OK);
+    @RequestMapping(path = "/api/streaming/test", method = { RequestMethod.GET })
+    public ResponseEntity<String> list(@RequestParam("message") String message, HttpServletRequest request) {
+        service.send(message);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
-
 
 }
