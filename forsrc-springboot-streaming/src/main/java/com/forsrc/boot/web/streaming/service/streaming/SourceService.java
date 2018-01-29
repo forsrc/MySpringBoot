0bce2a1410084fc2f5e14ce8cdd75826eb312ae0
forsrc <forsrc@gmail.com>
2018-01-29 22:35:51 +0900

    streaming

--- forsrc-springboot-streaming/src/main/java/com/forsrc/boot/web/streaming/service/streaming/SourceService.java
+++ forsrc-springboot-streaming/src/main/java/com/forsrc/boot/web/streaming/service/streaming/SourceService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
 @EnableBinding(Source.class)
+@Service
 public class SourceService {
 
     @Autowired
